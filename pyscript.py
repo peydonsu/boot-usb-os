66d8dde09369690f589435b3cd868a741ae0ce3c
peydonsu <supeydon41@gmail.com>
2026-06-09 11:48:34 -0600

    commit

---
+++ pyscript.py
@@ -0,0 +1,18 @@
+import os as Windows
+import cv2
+from tkinter import messagebox as x
+import playsound3 as playsound
+
+def dll():
+    img = cv2.imread("image.png")
+    cv2.imshow('image', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    
+def messageInfo(title, str):
+    x.showinfo(title, str)
+    playsound.playsound("Alarm01.wav")
+    
+def messageWarning(title, str):
+    x.showwarning(title, str)
+    playsound.playsound("Alarm01.wav")
---
+++ usb_menu.py
@@ -0,0 +1,8 @@
+import bot as b
+import pyscript as js
+
+js.dll()
+
+js.messageInfo("USB Drive", "Did you want to execute this file?")
+
+b.media_tools()
