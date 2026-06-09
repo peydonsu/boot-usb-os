1900d5fa17e9bf342d56f775ee281e4bc53eedbc
peydonsu <supeydon41@gmail.com>
2026-06-09 09:04:33 -0600

    read

--- bot.py
+++ bot.py
@@ -1,7 +1,9 @@
 import playsound3 as playsound
 
-playsound.playsound("Windows Background.wav")
-print("loading...")
 
-playsound.playsound("Windows Foreground.wav")
-print("fail")
+def media_tools():
+    playsound.playsound("Windows Background.wav")
+    print("loading...")
+
+    playsound.playsound("Windows Foreground.wav")
+    print("fail")
